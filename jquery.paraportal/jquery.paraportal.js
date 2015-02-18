2c44e1f0305ed830e8d170c7510de77e281f81b9
adhong <adam.hong@microsoft.com>
2015-02-18 14:22:22 -0500

    Created solution, namespacing under jquery

---
+++ jquery.paraportal/jquery.paraportal.js
@@ -0,0 +1,31 @@
+﻿if (jQuery) {
+    jQuery.paraportal = (new function() {
+        var readyQueue = [];
+        var isReady = false;
+
+        //Mimicking jquery ready functionality
+        this.ready = function (callback) {
+            if (isReady) {
+                //If the portal is alrady ready, just perform the callback
+                callback(jQuery);
+            } else {
+                //Queue it up
+                readyQueue.push(function () {
+                    callback(jQuery);
+                });
+            }
+        }
+
+        jQuery(document).ready(function ($) {
+            //Parse the dom
+
+
+            isReady = true;
+        });
+    }());   
+}
+
+jQuery.paraportal.ready(function ($) {
+    
+});
+    
