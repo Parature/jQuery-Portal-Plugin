be4843e1835d2b9ffe0041b22c75ca573571ddc7
adhong <adam.hong@microsoft.com>
2015-02-20 10:34:51 -0500

    Updated permissions. Added current page logic. Added ability to retrieve product table

--- jquery.paraportal/example.js
+++ jquery.paraportal/example.js
@@ -1,1 +1,4 @@
-﻿
+﻿jQuery.paraportal.ready(function ($) {
+    //Passing an object to your callback allows you to use it as your jQuery shorthand reference
+    
+});
--- jquery.paraportal/jquery.paraportal.js
+++ jquery.paraportal/jquery.paraportal.js
@@ -17,6 +17,20 @@
             }
         }
 
+        var permissions = {
+            "Knowledgebase" : "Knowledgebase",
+            "Download" : "Download",
+            "Chat" : "Chat",
+            "Ticket" : "Ticket",
+            "My Support" : "My Support",
+            "My Profile" : "My Profile",
+            "KB Subscribe" : "KB Subscribe",
+            "Product" : "Product",
+            "Email" : "Email",
+            "Glossary" : "Glossary",
+            "Troubleshooter" : "Troubleshooter" 
+        }
+
         //List of pages that we may use on the portal
         this.pages = {
             "splash": "splash",
@@ -42,39 +56,179 @@
             "myemails": "myemails",
         }
 
-        this.customer = {};
+        this.portalURLs = {
+            "/ics/support/splash.asp": this.pages.splash,
+            "/ics/support/default.asp": this.pages.splash,
+            "/ics/support/ticketnewwizard.asp": this.pages.ticketsubmit,
+            "/ics/support/srsplash.asp": this.pages.srsplash,
+            "/ics/support/ticketnewconfirm.asp": this.pages.ticketconfirm,
+            "/ics/support/dlsplash.asp": this.pages.downloadsplash,
+            "/ics/support/dllist.asp": this.pages.downloadfolder,
+            "/ics/support/kbsplash.asp": this.pages.kbsplash,
+            "/ics/support/kbanswer.asp": this.pages.kbarticle,
+            "/link/portal/\\d+/\\d+/article/\\d+/" : this.pages.kbarticle,
+            "/ics/support/kblist.asp": this.pages.kbfolder,
+            "/link/portal/\\d+/\\d+/articlefolder/\\d+/" : this.pages.kbfolder,
+            "/ics/support/ticketview.asp": this.pages.ticketdetails,
+            "/ics/support/ticketpreview.asp": this.pages.ticketpreview,
+            "/ics/support/kbresult.asp": this.pages.kbsearchresults,
+            "/ics/support/contactus.asp": this.pages.contactus,
+            "/ics/support/frameless/search.asp": this.pages.advancedsearch,
+            "/ics/support/mytickets.asp": this.pages.mytickets,
+            "/ics/support/myhistory.asp": this.pages.myhistory,
+            "/ics/support/mylogin.asp": this.pages.mylogin,
+            "/ics/support/myproducts.asp": this.pages.myproducts,
+            "/ics/support/mychats.asp": this.pages.mychats,
+            "/ics/support/mysubscriptions.asp": this.pages.mysubscriptions,
+            "/ics/support/myemails.asp": this.pages.myemails
+        }
 
-        var customer = this.customer;
+        this.customer = {};
+        this.permissions = [];
+
+        var paraportal = this;
 
         jQuery(document).ready(function ($) {
-            //Parse the dom for customer information
 
+            //Get the product table from the myproducts page and return the resulting object
+            paraportal.getProducts = function () {
+
+                var def = jQuery.Deferred();
+
+                if ($.inArray(permissions.Product, this.permissions) != -1) {
+                    $.get("/ics/support/myproducts.asp").done(function (data, status) {
+
+                        if ($('.errormessage', data).length > 0) {
+                            def.reject("No product access");
+                        } else {
+                            //Parse the product table into an object
+
+                            var products = [];
+                            var headers = [];
+                            $('.tableList th', data).each(function () {
+                                headers.push($.trim($(this).text()));
+                            });
+
+                            $('.tableList tr', data).each(function () {
+
+                                var product = {};
+
+                                if ($('th', this).length == 0) {
+                                    $('td', this).each(function () {
+                                        var index = $(this).index();
+                                        product[headers[index]] = $.trim($(this).html());
+                                    });
+                                    products.push(product);
+                                }
+                            });
+
+                            def.resolve(products);
+                        }
+                    }).fail(function (data, status) {
+                        def.reject('HTTP Error: ' + status);
+                    });
+                } else {
+                    def.reject("No product access");
+                }
+
+                return def;
+            }
+
+
+            //Get the customer fields from the customer page and return the resulting object
+            paraportal.getCustomerFields = function () {
+                var def = $.Deferred();
+
+                if ($.inArray(permissions["My Profile"], this.permissions) != -1) {
+                    $.get("/ics/support/myprofile.asp").done(function (data, status) {
+
+                        if ($('.errormessage', data).length > 0) {
+                            def.reject("Does not have My Profile access");
+                        } else {
+                            var customerFields = [];
+
+                            $('div[id*="ROW"]', data).each(function() {
+                                if ($('input[type="password"]').length == 0) {
+                                    var field = {};
+
+
+                                    customerFields.push(field);
+                                }                                
+                            });
+
+                            def.resolve(customerFields);
+                        }
+                    }).fail(function (data, status) {
+                        def.reject('HTTP Error: ' + status);
+                    });
+                } else {
+                    def.reject("Does not have My Profile access");
+                }
+
+                return def;
+            }
+
+            //Parse the dom for customer information
             loggedIn = $('#welcome_email').length > 0;
             if (loggedIn) {
-                customer.firstName = $('#welcome_firstname').text();
-                customer.lastName = $('#welcome_lastname').text();
-                customer.username = $('#welcome_username').text();
-                customer.email = $('#welcome_email').text();
+                paraportal.customer.firstName = $('#welcome_firstname').text();
+                paraportal.customer.lastName = $('#welcome_lastname').text();
+                paraportal.customer.username = $('#welcome_username').text();
+                paraportal.customer.email = $('#welcome_email').text();
                 
                 var numReg = /\d+/g;
                 var matches = $('#welcome_custnum').text().match(numReg);
                 if (matches) {
-                    customer.custNum = matches[0];
+                    paraportal.customer.custNum = matches[0];
                 } else {
-                    customer.custNum = (function() {
+                    paraportal.customer.custNum = (function() {
                         throw "Configuration Issue: Customer Number is not available. Please make sure that the Customer Number static field is present and that the field is not set to internal.";
                     })();
                 }
             }
 
-            customer.isAuthenticated = loggedIn;
+            paraportal.customer.isAuthenticated = loggedIn;
+
+            //Permissions are exposed via the portal submenus...
+            var menuToPermissions = {
+                "#mli_knowledge": permissions["Knowledgebase"],
+                "#mli_download": permissions["Download"],
+                "#mli_realtime": permissions["Chat"],
+                "#mli_ticket": permissions["Ticket"],
+                "#mli_myHistory" : permissions["My Support"],
+                "#mli_mySubscriptions": permissions["KB Subscribe"],
+                "#mli_myProducts": permissions.Product,
+                "#mly_myProfile" : permissions["My Profile"],
+                "#mli_email": permissions["Email"],
+                "#mli_glossary": permissions["Glossary"],
+                "#mli_troubleshooter": permissions["Troubleshooter"] //FYI Troubleshooter is sub-optimal, just use EasyAnswer
+            };
+
+            for (var id in menuToPermissions) {
+                if ($('.submenu ' + id).length > 0)
+                    paraportal.permissions.push(menuToPermissions[id]);
+            }
+
+            for (var url in paraportal.portalURLs) {
+                var regex = new RegExp(url);
+                if (regex.test(window.location.pathname.toLowerCase())) {
+                    paraportal.currentPage = paraportal.portalURLs[url];
+                    break;
+                }
+            }
+
+            //If its some page that we haven't accounted for, just return the path
+            if (!paraportal.currentPage) {
+                paraportal.currentPage = window.location.pathname.toLowerCase();
+            }
 
             isReady = true;
+
+            //Execute any ready callbacks
+            for (var i = 0; i < readyQueue.length; i++) {
+                var callback = readyQueue[i];
+                callback(jQuery);
+            }
         });
     }());
 }
-
-jQuery.paraportal.ready(function ($) {
-    
-});
-    
